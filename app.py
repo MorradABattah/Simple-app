38e90ce42dbfe6be6326b607cf070d35652335c4
MorradABattah <morradbattah1995@gmail.com>
2023-07-26 19:31:07 -0700

    Update app.py

--- app.py
+++ app.py
@@ -37,4 +37,4 @@ def time():
     return render_template('time.html', time=formatted_time)
 
 if __name__ == "__main__":
-    app.run(debug=False, host='127.0.0.1')
+    app.run(debug=False, host='0.0.0.0')
